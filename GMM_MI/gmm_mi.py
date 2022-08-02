1890abdee50b90a3da3bbc9d1ad6c437448bf087
Davide Piras <d.piras@ucl.ac.uk>
2022-08-02 12:45:35 +0100

    Clean tree, start separating classes and utils

---
+++ GMM_MI/gmm_mi.py
@@ -0,0 +1,363 @@
+import numpy as np
+from scipy.stats import multivariate_normal
+from scipy.special import logsumexp
+from scipy import linalg
+import scipy.integrate as integrate
+
+from sklearn.mixture import GaussianMixture as GMM
+from sklearn.mixture._gaussian_mixture import _estimate_log_gaussian_prob, _compute_precision_cholesky, _estimate_gaussian_covariances_full
+from sklearn.utils import check_random_state
+from sklearn import cluster
+from sklearn.model_selection import KFold
+
+def log_pdf(y, x, model):
+    y = np.array(y)
+    x = np.array(x)
+    X = np.concatenate((y.reshape(1, 1), x.reshape(1, 1))).T
+    return model.score_samples(X)
+
+def pdf(y, x):
+    return np.exp(log_pdf(y, x))
+
+def entropy_2d_integrand(y, x, model):
+    """
+    TODO
+    """
+    logp = log_pdf(y, x, model)
+    p = np.exp(logp)
+    return p*logp
+
+def entropy_1d_integrand(x, model, index):
+    """
+    TODO
+    """
+    x = np.array(x)
+    w = model.weights_
+    m = model.means_[:, index:index+1]
+    c = model.covariances_[:, index:index+1, index:index+1]
+    gmm_marginal = GMM_MI(n_components=len(w), weights_init=w, means_init=m, covariances_init=c)
+    logp_1d = gmm_marginal.score_samples(x.reshape(-1, 1))
+    p = np.exp(logp_1d)
+    return p*logp_1d
+
+class GMM_MI(GMM):
+    """
+    Custom GMM class based on the sklearn GMM class.
+    This allows to work with a GMM with fixed parameters, without fitting it.
+    It also allows to estimate MI with a certain number of MC samples, or with quadrature method.
+    The different initialisation types are dealt with separately.
+    """
+    def __init__(self,
+                 n_components=1,
+                 covariance_type="full",
+                 tol=1e-5,
+                 reg_covar=1e-6,
+                 max_iter=100,
+                 n_init=1,
+                 init_params="random",
+                 random_state=None,
+                 warm_start=False,
+                 verbose=0,
+                 verbose_interval=10,
+                 weights_init=None,
+                 means_init=None,
+                 precisions_init=None,
+                 covariances_init=None
+                 ):
+        super(GMM_MI, self).__init__(n_components=n_components,
+                 covariance_type=covariance_type,
+                 tol=tol,
+                 reg_covar=reg_covar,
+                 max_iter=max_iter,
+                 n_init=n_init,
+                 init_params=init_params,
+                 random_state=random_state,
+                 warm_start=warm_start,
+                 verbose=verbose,
+                 verbose_interval=verbose_interval,
+                 weights_init=weights_init,
+                 means_init=means_init,
+                 precisions_init=precisions_init,
+                )
+
+        self.means_ = means_init
+        if covariances_init is None:
+            covariances_init = np.linalg.inv(self.precisions_init)
+        self.covariances_ = covariances_init
+        self.covariances_init = covariances_init
+        self.weights_ = weights_init
+        self.covariance_type = covariance_type
+        self.precisions_cholesky_ = _compute_precision_cholesky(
+                self.covariances_, self.covariance_type
+            )
+
+
+    def score_samples(self, X):
+        """Compute the log-likelihood of each sample.
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            List of n_features-dimensional data points. Each row
+            corresponds to a single data point.
+        Returns
+        -------
+        log_prob : array, shape (n_samples,)
+            Log-likelihood of each sample in `X` under the current model.
+        """
+
+        return logsumexp(self._estimate_weighted_log_prob(X), axis=1)
+
+    def predict(self, X):
+        """Predict the labels for the data samples in X using trained model.
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            List of n_features-dimensional data points. Each row
+            corresponds to a single data point.
+        Returns
+        -------
+        labels : array, shape (n_samples,)
+            Component labels.
+        """
+
+        return self._estimate_weighted_log_prob(X).argmax(axis=1)
+
+    def predict_proba(self, X):
+        """Evaluate the components' density for each sample.
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            List of n_features-dimensional data points. Each row
+            corresponds to a single data point.
+        Returns
+        -------
+        resp : array, shape (n_samples, n_components)
+            Density of each Gaussian component for each sample in X.
+        """
+        _, log_resp = self._estimate_log_prob_resp(X)
+        return np.exp(log_resp)
+
+    def sample(self, n_samples=1):
+        """Generate random samples from the fitted Gaussian distribution.
+        Parameters
+        ----------
+        n_samples : int, default=1
+            Number of samples to generate.
+        Returns
+        -------
+        X : array, shape (n_samples, n_features)
+            Randomly generated sample.
+        y : array, shape (nsamples,)
+            Component labels.
+        """
+
+        if n_samples < 1:
+            raise ValueError(
+                "Invalid value for 'n_samples': %d . The sampling requires at "
+                "least one sample." % (self.n_components)
+            )
+
+        _, n_features = self.means_.shape
+        rng = check_random_state(self.random_state)
+        n_samples_comp = rng.multinomial(n_samples, self.weights_)
+
+        if self.covariance_type == "full":
+            X = np.vstack(
+                [
+                    rng.multivariate_normal(mean, covariance, int(sample))
+                    for (mean, covariance, sample) in zip(
+                        self.means_, self.covariances_, n_samples_comp
+                    )
+                ]
+            )
+        elif self.covariance_type == "tied":
+            X = np.vstack(
+                [
+                    rng.multivariate_normal(mean, self.covariances_, int(sample))
+                    for (mean, sample) in zip(self.means_, n_samples_comp)
+                ]
+            )
+        else:
+            X = np.vstack(
+                [
+                    mean + rng.randn(sample, n_features) * np.sqrt(covariance)
+                    for (mean, covariance, sample) in zip(
+                        self.means_, self.covariances_, n_samples_comp
+                    )
+                ]
+            )
+
+        y = np.concatenate(
+            [np.full(sample, j, dtype=int) for j, sample in enumerate(n_samples_comp)]
+        )
+
+        return (X, y)
+
+    def score_samples_marginal(self, X, index=0):
+        """Compute the log-likelihood of each sample for the marginal model, indexed by either 0 (x) or 1 (y).
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            List of n_features-dimensional data points. Each row
+            corresponds to a single data point.
+        index: integer
+            Either 0 (marginal x) or 1 (marginal y).
+        Returns
+        -------
+        log_prob : array, shape (n_samples,)
+            Log-likelihood of each sample in `X` under the current model.
+        """
+
+        oned_cholesky = np.sqrt(1/self.covariances_[:, index, index]).reshape(-1, 1, 1)
+        marginal_logprob = _estimate_log_gaussian_prob(
+            X, self.means_[:, index].reshape(-1, 1), oned_cholesky, self.covariance_type
+        )
+
+        return logsumexp(np.log(self.weights_) + marginal_logprob, axis=1)
+
+    
+    def fit_predict(self, X, y=None):
+        """Estimate model parameters using X and predict the labels for X.
+        The method fits the model n_init times and sets the parameters with
+        which the model has the largest likelihood or lower bound. Within each
+        trial, the method iterates between E-step and M-step for `max_iter`
+        times until the change of likelihood or lower bound is less than
+        `tol`, otherwise, a :class:`~sklearn.exceptions.ConvergenceWarning` is
+        raised. After fitting, it predicts the most probable label for the
+        input data points.
+        .. versionadded:: 0.20
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            List of n_features-dimensional data points. Each row
+            corresponds to a single data point.
+        y : Ignored
+            Not used, present for API consistency by convention.
+        Returns
+        -------
+        labels : array, shape (n_samples,)
+            Component labels.
+        """
+        X = self._validate_data(X, dtype=[np.float64, np.float32], ensure_min_samples=2)
+        if X.shape[0] < self.n_components:
+            raise ValueError(
+                "Expected n_samples >= n_components "
+                f"but got n_components = {self.n_components}, "
+                f"n_samples = {X.shape[0]}"
+            )
+        self._check_initial_parameters(X)
+
+        # if we enable warm_start, we will have a unique initialisation
+        do_init = not (self.warm_start and hasattr(self, "converged_"))
+        n_init = self.n_init if do_init else 1
+
+        max_lower_bound = -np.inf
+        self.converged_ = False
+
+        random_state = check_random_state(self.random_state)
+
+        n_samples, _ = X.shape
+        for init in range(n_init):
+            self._print_verbose_msg_init_beg(init)
+
+            if do_init:
+                self._initialize_parameters(X, random_state)
+
+            lower_bound = -np.inf if do_init else self.lower_bound_
+
+            for n_iter in range(1, self.max_iter + 1):
+
+                prev_lower_bound = lower_bound
+
+                log_prob_norm, log_resp = self._e_step(X)
+                self._m_step(X, log_resp)
+                lower_bound = self._compute_lower_bound(log_resp, log_prob_norm)
+
+                change = lower_bound - prev_lower_bound
+                self._print_verbose_msg_iter_end(n_iter, change)
+
+                if abs(change) < self.tol:
+                    self.converged_ = True
+                    break
+
+            self._print_verbose_msg_init_end(lower_bound)
+
+            if lower_bound > max_lower_bound or max_lower_bound == -np.inf:
+                max_lower_bound = lower_bound
+                best_params = self._get_parameters()
+                best_n_iter = n_iter
+
+        if not self.converged_:
+            warnings.warn(
+                "Initialization %d did not converge. "
+                "Try different init parameters, "
+                "or increase max_iter, tol "
+                "or check for degenerate data." % (init + 1),
+                ConvergenceWarning,
+            )
+
+        self._set_parameters(best_params)
+        self.n_iter_ = best_n_iter
+        self.lower_bound_ = max_lower_bound
+
+        # Always do a final e-step to guarantee that the labels returned by
+        # fit_predict(X) are always consistent with fit(X).predict(X)
+        # for any value of max_iter and tol (and any random_state).
+        _, log_resp = self._e_step(X)
+
+        return log_resp.argmax(axis=1)
+        
+    def estimate_MI_MC(self, MC_samples=1e3):
+        """
+        Compute the mutual information (MI) associated with a particular GMM model, using MC integration
+        Parameters
+        ----------
+        MC_samples : integer
+            Number of Monte Carlo samples to perform numerical integration of the MI integral.
+        Returns
+        ----------
+        MI : integer
+            The value of mutual information.
+        -------
+        """
+        # sample MC samples
+        points, clusters = self.sample(MC_samples)
+        
+        # we first evaluate the log-likelihood for the joint probability
+        joint = self.score_samples(points)
+
+        # we then evaluate the marginals; index=0 corresponds to x, index=y corresponds to y
+        marginal_x = self.score_samples_marginal(points[:, :1], index=0)
+        marginal_y = self.score_samples_marginal(points[:, 1:], index=1)
+
+        MI = np.mean(joint - marginal_x - marginal_y)
+        return MI
+
+    def estimate_MI_quad(self, tol_int=1.49e-8, limit = np.inf):
+        """
+        TO DO:
+        Compute the mutual information (MI) associated with a particular GMM model, using quadrature integration.
+        Parameters
+        ----------
+        MC_samples : integer
+            Number of Monte Carlo samples to perform numerical integration of the MI integral.
+        Returns
+        ----------
+        MI : integer
+            The value of mutual information.
+        -------
+        """
+        gmm = GMM_MI(n_components=self.n_components, weights_init=self.weights_, 
+                     means_init=self.means_, covariances_init=self.covariances_)
+        
+        entropy_2d = integrate.dblquad(entropy_2d_integrand, -limit, limit, 
+                                      lambda x: -limit, lambda x: limit, 
+                                      args=[gmm], epsabs=tol_int, epsrel=tol_int)[0]
+        entropy_1d_x = integrate.quad(entropy_1d_integrand, -limit, limit, 
+                                      args=(gmm, 0), epsabs=tol_int, epsrel=tol_int)[0]
+        entropy_1d_y = integrate.quad(entropy_1d_integrand, -limit, limit, 
+                                      args=(gmm, 1), epsabs=tol_int, epsrel=tol_int)[0]
+
+        MI = entropy_2d - entropy_1d_x - entropy_1d_y
+        return MI
+    
---
+++ GMM_MI/utils/synthetic_data.py
@@ -0,0 +1,46 @@
+import numpy as np
+from gmm_mi import GMM_MI
+random_state = 13
+
+# we define a few different synthetic models, with fixed random state
+D1 = GMM_MI(n_components = 1,
+            weights_init = np.array([1.0]),
+            means_init = np.array([[-1, 1]]),
+            covariances_init = np.array([[[0.5, 0.2], [0.2, 0.5]]]),
+            random_state = random_state)
+ 
+D3 = GMM_MI(n_components = 3,
+            weights_init = np.array([0.3, 0.45, 0.25]),
+            means_init = np.array([[-1, 1], [0, 2], [-1.5, 2]]),
+            covariances_init = np.array([[[1, 0], [0, 0.1]], 
+                                         [[0.5, 0.2], [0.2, 0.5]], 
+                                         [[0.1, 0.05], [0.05, 0.1]]]),
+            random_state = random_state)
+ 
+D3p = GMM_MI(n_components = 3,
+            weights_init = np.array([0.3, 0.45, 0.25]),
+            means_init = np.array([[-3, 3], [2, 4], [-2.5, 5]]),
+            covariances_init = np.array([[[1, 0], [0, 0.1]], 
+                                         [[0.5, 0.2], [0.2, 0.5]], 
+                                         [[0.1, 0.05], [0.05, 0.1]]]),
+            random_state = random_state)
+
+D5 = GMM_MI(n_components = 5,
+            weights_init = np.array([0.2, 0.35, 0.15, 0.12, 0.18]),
+            means_init = np.array([[-1, 1], [0, 2], [-1.5, 2], [2, 1], [-0.25, 0]]),
+            covariances_init = np.array([[[1, 0], [0, 0.1]], 
+                                         [[0.5, 0.2], [0.2, 0.5]], 
+                                         [[0.1, 0.05], [0.05, 0.1]], 
+                                         [[0.5, -0.1], [-0.1, 0.9]], 
+                                         [[0.2, -0.05], [-0.05, 0.1]]]),
+            random_state = random_state)
+
+D5p = GMM_MI(n_components = 5,
+            weights_init = np.array([0.2, 0.35, 0.15, 0.12, 0.18]),
+            means_init = np.array([[-1, 1], [0, 2], [-1.5, 2], [3, 1], [-0.25, 0]]),
+            covariances_init = np.array([[[1, 0], [0, 0.1]], 
+                                         [[0.5, 0.2], [0.2, 0.5]], 
+                                         [[0.1, 0.05], [0.05, 0.1]], 
+                                         [[0.5, -0.1], [-0.1, 0.9]], 
+                                         [[0.2, -0.05], [-0.05, 0.1]]]),
+            random_state = random_state)
