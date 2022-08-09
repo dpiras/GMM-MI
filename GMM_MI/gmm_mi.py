72673ff9b13964cdda3980d9fe8e116d19a17fa6
Davide Piras <d.piras@ucl.ac.uk>
2022-08-09 11:08:15 +0100

    Finish docstrings of gmm_mi.py

--- GMM_MI/gmm_mi.py
+++ GMM_MI/gmm_mi.py
@@ -167,7 +167,8 @@ def single_fit(X, n_components, reg_covar=1e-15, tol=1e-6, max_iter=10000,
         The maximum number of iterations in each GMM fit. We aim to stop only based on the tolerance, 
         so it is set to a high value.    
     random_state : int, default=None
-        Random seed used to initialise the GMM model. Not used if initial GMM parameters are provided.
+        Random seed used to initialise the GMM model. 
+        If initial GMM parameters are provided, used only to fix the trained model samples across trials.
     w_init : array-like of shape (n_components)
         Initial GMM weights.
     m_init : array-like of shape (n_components, 2)
@@ -189,9 +190,9 @@ def single_fit(X, n_components, reg_covar=1e-15, tol=1e-6, max_iter=10000,
     return gmm
  
     
-def select_best_metric(X, current_results_dict, select_c, 
-                       n_components, init_type,
-                      tol, max_iter):
+def select_best_metric(X, results_dict, n_components,
+                       select_c='valid', init_type='random_sklearn',
+                       reg_covar=1e-15, tol=1e-6, max_iter=10000):
     """
     Select best metric to choose the number of GMM components.
 
@@ -199,8 +200,16 @@ def select_best_metric(X, current_results_dict, select_c,
     ----------
     X : array-like of shape (n_samples, 2)
         Samples from the joint distribution of the two variables whose MI is calculated.
-    current_results_dict : 
-    
+    results_dict : dictionary
+        Contains all the results from cross-validation.
+    n_components : int
+        Number of GMM components to fit.           
+    select_c : {'valid', 'aic', 'bic'}, default='valid'
+        Method used to select the optimal number of GMM components to perform density estimation.
+        Must be one of:
+            'valid': stop adding components when the validation log-likelihood stops increasing.
+            'aic': stop adding components when the Akaike information criterion (AIC) stops decreasing
+            'bic': same as 'aic' but with the Bayesian information criterion (BIC).    
     init_type : {'random', 'minmax', 'kmeans', 'random_sklearn', 'kmeans_sklearn'}, default='random_sklearn'
         The method used to initialize the weights, the means, the covariances and the precisions in each CV fit.
         See utils.initializations for more details.
@@ -211,55 +220,106 @@ def select_best_metric(X, current_results_dict, select_c,
     max_iter : int, default=10000
         The maximum number of iterations in each GMM fit. We aim to stop only based on the tolerance, 
         so it is set to a high value.      
-    select_c : {'valid', 'aic', 'bic'}, default='valid'
-        Method used to select the optimal number of GMM components to perform density estimation.
-        Must be one of:
-            'valid': stop adding components when the validation log-likelihood stops increasing.
-            'aic': stop adding components when the Akaike information criterion (AIC) stops decreasing
-            'bic': same as 'aic' but with the Bayesian information criterion (BIC).
-
         
     Returns
     ----------
     metric : float
-        
+        The value of the metric selected to choose the number of components.
     """    
-    # if we want to select the number of components based on AIC or BIC, we need to change the metric
     if select_c == 'aic' or select_c == 'bic':
-        current_seed = current_results_dict['best_seed']
-        w_init, m_init, c_init, p_init = initialize_parameters(X, random_state=current_seed, 
-                                                               n_components=n_components, init_type=init_type)
-        gmm = single_fit(X, n_components=n_components, reg_covar=reg_covar, 
-                tol=tol, max_iter=max_iter, w_init=w_init, 
-                m_init=m_init, p_init=p_init)                
+        # in this case we need to re-fit the dataset; we start from the final point
+        _, _, w_init, m_init, p_init, _ = extract_best_parameters(n_components=n_components, 
+                                                               fixed_components=False, 
+                                                               patience=0, 
+                                                               results_dict=results_dict)
+        gmm = single_fit(X=X, n_components=n_components, reg_covar=reg_covar, 
+                         tol=tol, max_iter=max_iter, w_init=w_init, 
+                         m_init=m_init, p_init=p_init)                
         if select_c == 'aic':
-            # explain why we use the negative metric for AIC and BIC
+            # negative since we maximise the metric
             metric = -gmm.aic(X)
         elif select_c == 'bic':
             metric = -gmm.bic(X)
     elif select_c == 'valid':
-        metric = current_results_dict['best_val_score']
+        metric = results_dict[n_components]['best_val_score']
+    else:
+        raise ValueError(f"select_c must be either 'valid', 'aic' or 'bic, found '{select_c}'")
     return metric
 
 def check_convergence(metric, best_metric, n_components, patience, patience_counter):
+    """
+    Check if convergence with respect to the number of components has been reached.
+
+    Parameters
+    ----------
+    metric : float
+        Current metric value, to choose convergence.
+    best_metric : float
+        Current best metric value.
+    n_components : int
+        Number of GMM components being fitted. 
+    patience : int
+        Number of extra components to "wait" until convergence is declared.
+    patience_counter : int
+        Counter to keep track of how many components we added so far with respect to the patience.
+    
+    Returns
+    ----------
+    converged : bool
+        Whether we reached the optimal number of GMM components.
+    best_metric : float
+        Updated best metric.
+    patience_counter : int
+        Updated patience counter.
+    """    
     converged = False
     if metric > best_metric:
         best_metric = metric
-        print(n_components, best_metric)
+        print(f'Current components: {n_components}. Current metric: {best_metric:.3f}. Adding one component...')
     else:
         patience_counter += 1
-        # might need to add a message here...
+        print(f'Metric did not improve; increasing patience counter...')
         if patience_counter >= patience:
             converged = True
     return converged, best_metric, patience_counter
 
-def find_best_parameters(n_components, fixed_components, patience, results_dict):
+def extract_best_parameters(results_dict, n_components, fixed_components, patience):
+    """
+    Extract best parameters relative to cross-validation.
+    These parameters are then used to initialize the final MI estimation.
+
+    Parameters
+    ----------
+    results_dict : dictionary
+        Contains all the results from cross-validation.
+    n_components : int
+        Number of GMM components to fit. 
+    fixed_components : bool
+        Fix the number of GMM components to use for density estimation.
+        In this instance, only used to decide if patience is used or not.
+    patience : int
+        Number of extra components to "wait" until convergence is declared. 
+        Decides how far back to look to select the number of components. Only used if fixed_components is not True.
+    
+    Returns
+    ----------
+    best_components : int
+        Best number of components to fit (either the input ones, or fewer if it's during patience).
+    best_seed : int
+        Best seed of the initialisation that led to the highest validation log-likelihood.
+    w_init : array-like of shape (n_components)
+        Best GMM weights (based on cross-validation), to be used as initial point of further fits.
+    m_init : array-like of shape (n_components, 2)
+        Best GMM means (based on cross-validation), to be used as initial point of further fits.
+    p_init : array-like of shape ( n_components, 2, 2)
+        Best GMM precisions (based on cross-validation), to be used as initial point of further fits.
+    lcurves : list of lists
+        Filled list with all loss curves from cross-validation. 
+    """    
     best_components = n_components
     if not fixed_components:
         best_components -= patience
-    # first we need to retrieve the best results
-    print(f'Convergence reached at {best_components} components') 
-    loss_curves = results_dict[best_components]['all_lcurves']
+    lcurves = results_dict[best_components]['all_lcurves']
     best_seed = results_dict[best_components]['best_seed']
     best_fold_in_init = results_dict[best_components]['best_fold_in_init']            
     all_ws = results_dict[best_components]['all_ws']
@@ -268,10 +328,34 @@ def find_best_parameters(n_components, fixed_components, patience, results_dict)
     w_init = all_ws[best_seed, best_fold_in_init]
     m_init = all_ms[best_seed, best_fold_in_init]
     p_init = all_ps[best_seed, best_fold_in_init] 
-    return best_components, best_seed, w_init, m_init, p_init, loss_curves
+    return best_components, best_seed, w_init, m_init, p_init, lcurves
 
 
 def calculate_MI(gmm, MI_method='MC', MC_samples=1e5, tol_int=1.49e-8, limit=np.inf):
+    """
+    Calculate mutual information (MI) integral given a Gaussian mixture model in 2D.
+    Use either Monte Carlo (MC) method, or quadrature method.
+
+    Parameters
+    ----------
+    gmm : instance of GMM class
+        The GMM model whose MI we calculate.
+    MI_method : {'MC', 'quad'}, default='MC'
+        Whether to use MC ('MC') method or quadrature ('quad') method to estimate the integral.
+    MC_samples : int, default=1e5
+        Number of MC samples used to estimate integral. Only used when MI_method='MC'.
+    tol_int : float, default=1.49e-8
+        Integral tolerance; the default value is the one form scipy. 
+        Only used when MI_method='quad'.
+    limit : float, default=np.inf
+        The extrema of the integral to calculate. Usually the whole plane, so defaults to inf.
+        Only used when MI_method='quad'.
+
+    Returns
+    ----------
+    MI : float
+        The value of MI.
+    """    
     if MI_method == 'MC':
         MI = gmm.estimate_MI_MC(MC_samples=MC_samples)
     elif MI_method == 'quad':
@@ -281,22 +365,57 @@ def calculate_MI(gmm, MI_method='MC', MC_samples=1e5, tol_int=1.49e-8, limit=np.
 
 def perform_bootstrap(X, n_bootstrap, n_components, 
                       reg_covar, tol, max_iter, random_state,
-                      w_init, m_init, p_init, MC_samples, MI_method='MC'):
+                      w_init, m_init, p_init, MI_method='MC', MC_samples=1e5):
     """
-    TODO
-    """
+    Perform bootstrap on the given data to calculate the distribution of mutual information (MI).
+
+    Parameters
+    ----------
+    X : array-like of shape (n_samples, 2)
+        Data over which bootstrap is performed.
+    n_bootstrap : int
+        Number of bootstrap realisations to consider to obtain the MI uncertainty.
+    n_components : int
+        Number of GMM components to fit.
+    reg_covar : float
+        The constant term added to the diagonal of the covariance matrices to avoid singularities.
+    tol : float
+        The log-likelihood threshold on each GMM fit used to choose when to stop training.
+    max_iter : int
+        The maximum number of iterations in each GMM fit. We aim to stop only based on the tolerance, 
+        so it is set to a high value.    
+    random_state : int, default=None
+        Random seed used to initialise the GMM model. 
+        If initial GMM parameters are provided, used only to fix the trained model samples across trials.
+    w_init : array-like of shape (n_components)
+        Initial GMM weights.
+    m_init : array-like of shape (n_components, 2)
+        Initial GMM means.
+    p_init : array-like of shape ( n_components, 2, 2)
+        Initial GMM precisions.
+    MI_method : {'MC', 'quad'}, default='MC'
+        Whether to use MC ('MC') method or quadrature ('quad') method to estimate the integral.
+    MC_samples : int, default=1e5
+        Number of MC samples used to estimate integral. Only used when MI_method='MC'.
+
+    Returns
+    ----------
+    MI_mean : float
+        Mean of the MI distribution.
+    MI_std : float
+        Standard deviation of the MI distribution.
+    """  
     MI_estimates = np.zeros(n_bootstrap)
-    # bootstrap available samples
-    for i in range(n_bootstrap):
-        # we use i to change the seed so that the results will be fully reproducible
-        rng = np.random.default_rng(i)
+    for n_b in range(n_bootstrap):
+        # we use index n_b to change the seed so that results will be fully reproducible
+        rng = np.random.default_rng(n_b)
         X_bs = rng.choice(X, X.shape[0])
-        gmm = single_fit(X_bs, n_components=n_components, reg_covar=reg_covar, 
+        gmm = single_fit(X=X_bs, n_components=n_components, reg_covar=reg_covar, 
                     tol=tol, max_iter=max_iter, 
                     random_state=random_state, w_init=w_init, 
                     m_init=m_init, p_init=p_init)
         current_MI_estimate = calculate_MI(gmm, MI_method=MI_method, MC_samples=MC_samples)
-        MI_estimates[i] = current_MI_estimate
+        MI_estimates[n_b] = current_MI_estimate
     MI_mean = np.mean(MI_estimates)
     MI_std = np.sqrt(np.var(MI_estimates, ddof=1))
     return MI_mean, MI_std
@@ -347,7 +466,8 @@ def GMM_MI(X, n_folds=3, n_inits=5, init_type='random_sklearn', reg_covar=1e-15,
         If False, only a single value of MI is returned.
     n_bootstrap : int, default=100 
         Number of bootstrap realisations to consider to obtain the MI uncertainty.
-    fixed_components : bool, default=False 
+    
+    ed_components : bool, default=False 
         Fix the number of GMM components to use for density estimation.
         For debugging purposes, or when you are sure of how many components to use.
     fixed_components_number : int, default=1
@@ -371,10 +491,8 @@ def GMM_MI(X, n_folds=3, n_inits=5, init_type='random_sklearn', reg_covar=1e-15,
     converged = False
     best_metric = -np.inf
     patience_counter = 0
-    results_dict = {}
-    
-    assert select_c == 'valid' or select_c == 'aic' or select_c == 'bic', f"select_c must be either 'valid', 'aic' or 'bic, found {select_c}"
-    
+    results_dict = {}    
+    assert select_c == 'valid' or select_c == 'aic' or select_c == 'bic', f"select_c must be either 'valid', 'aic' or 'bic, found '{select_c}'"
     for n_components in range(1, max_components+1):
         if fixed_components:
             if n_components < fixed_components_number:
@@ -383,36 +501,33 @@ def GMM_MI(X, n_folds=3, n_inits=5, init_type='random_sklearn', reg_covar=1e-15,
                 converged = True
         current_results_dict = cross_validation(X=X, n_components=n_components, n_folds=n_folds, max_iter=max_iter,
                                        init_type=init_type, n_inits=n_inits, tol=tol, reg_covar=reg_covar)
-        results_dict[n_components] = current_results_dict        
+        results_dict[n_components] = current_results_dict
         if not converged:
-            metric = select_best_metric(X=X, current_results_dict=current_results_dict, select_c=select_c,
+            metric = select_best_metric(X=X, results_dict=results_dict, select_c=select_c,
                                         n_components=n_components, init_type=init_type, tol=tol, max_iter=max_iter)
-            # check if convergence has been reached based on metric, and save current results
             converged, best_metric, patience_counter = check_convergence(metric=metric, best_metric=best_metric, 
                                                                          n_components=n_components, 
                                                                          patience=patience,
                                                                          patience_counter=patience_counter)
                
         if converged:
-            # if we found the optimal number of GMM components, 
-            # then we should stop and calculate MI with the previous parameters
-            best_components, best_seed, w_init, m_init, p_init, loss_curves = find_best_parameters(n_components=n_components,    
-                                                                                                   fixed_components=fixed_components,
-                                                                                                   patience=patience, 
-                                                                                                   results_dict=results_dict)
-            
+            best_components, best_seed, w_init, m_init, p_init, lcurves = extract_best_parameters(results_dict=results_dict,
+                                                                                                  n_components=n_components,    
+                                                                                                  fixed_components=fixed_components,
+                                                                                                  patience=patience)
+            print(f'Convergence reached at {best_components} components') 
             if bootstrap:
-                MI_mean, MI_std = perform_bootstrap(X, n_bootstrap=n_bootstrap, n_components=best_components, 
+                MI_mean, MI_std = perform_bootstrap(X=X, n_bootstrap=n_bootstrap, n_components=best_components, 
                                                     reg_covar=reg_covar, tol=tol, max_iter=max_iter, 
-                                                    random_state=best_seed, MC_samples=MC_samples,
-                                                    w_init=w_init, m_init=m_init, p_init=p_init)
+                                                    random_state=best_seed, MI_method=MI_method, 
+                                                    MC_samples=MC_samples, w_init=w_init, m_init=m_init, p_init=p_init)
             else:
                 # only now while debugging, we perform a single fit on the entire dataset
-                gmm = single_fit(X, n_components=best_components, reg_covar=reg_covar, 
-                            tol=tol, max_iter=max_iter, random_state=best_seed, w_init=w_init, 
-                            m_init=m_init, p_init=p_init)
+                gmm = single_fit(X=X, n_components=best_components, reg_covar=reg_covar, 
+                                 tol=tol, random_state=best_seed, max_iter=max_iter, 
+                                 w_init=w_init, m_init=m_init, p_init=p_init)
                 MI_mean = calculate_MI(gmm, MI_method=MI_method, MC_samples=MC_samples)
                 MI_std = None
             break
             
-    return MI_mean, MI_std, loss_curves
+    return MI_mean, MI_std, lcurves
