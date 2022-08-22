7c421147194ac0d20a3e7f33af452b23eea2a4ba
Davide Piras <d.piras@ucl.ac.uk>
2022-08-22 11:13:25 +0100

    6th notebook

--- gmm_mi/utils/analytic_MI.py
+++ gmm_mi/utils/analytic_MI.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import psi 
 
 
 def calculate_MI_D1_analytical(covariance_matrix):
@@ -18,3 +19,41 @@ def calculate_MI_D1_analytical(covariance_matrix):
     assert covariance_matrix.shape==(2, 2), "Covariance matrix must be of shape (2, 2)."
     MI = -0.5*np.log(1-(covariance_matrix[0, 1] / (np.sqrt(covariance_matrix[0, 0]*covariance_matrix[1, 1])))**2)
     return MI
+
+
+def calculate_MI_gammaexp_analytical(alpha):
+    """Calculate the mutual information (MI) for a gamma-exponential distribution, with free parameter \alpha.
+    
+    Parameters
+    ----------
+    alpha : float
+        Free parameter of the distribution.
+    
+    Returns
+    ----------
+    MI : float
+        The value of mutual information in nats.
+    """
+    # psi is the digamma function
+    return psi(alpha+1) - np.log(alpha)
+
+
+def calculate_MI_weinman_analytical(alpha):
+    """Calculate the mutual information (MI) for an ordered Weinman exponential distribution, with free parameter \alpha.
+    
+    Parameters
+    ----------
+    alpha : float
+        Free parameter of the distribution.
+    
+    Returns
+    ----------
+    MI : float
+        The value of mutual information in nats.
+    """
+    if 0 < alpha < 0.5:
+        return  -np.log(2*alpha / (1-2*alpha)) + psi( 1 / (1-2*alpha)) -psi(1)
+    elif theta == 0.5:
+        return -psi(1)
+    else:
+        return  np.log( (2*alpha-1)  / (2*alpha)) + psi( (2*alpha) / (2*alpha-1)) -psi(1)
