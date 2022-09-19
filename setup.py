8183a103b89e6d63aadb4e6df771a17847132b61
Davide Piras <d.piras@ucl.ac.uk>
2022-09-19 15:40:29 +0100

    Switch to minimum versions and python 3.9

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='gmm_mi',
     version="0.1.0",
     author='Davide Piras',
-    author_email='d.piras@ucl.ac.uk',
+    author_email='dr.davide.piras@gmail.com',
     description='Estimate mutual information distribution with Gaussian mixture models',
     url='https://github.com/dpiras/MI_estimation',
     license='GNU General Public License v3.0 (GPLv3)',
@@ -17,13 +17,14 @@ setup(
         "License :: OSI Approved :: GPLv3 License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.9',
     install_requires=['pytest',
-                      'matplotlib==3.1.2',
-                      'numpy==1.17.4',
-                      'pandas==1.3.4',
-                      'scikit-learn==1.0.2',
-                      'scipy==1.7.1']
+                      'tqdm',
+                      'matplotlib>=3.1.2',
+                      'numpy>=1.17.4',
+                      'pandas>=1.3.4',
+                      'scikit-learn>=1.0.2',
+                      'scipy>=1.7.1']
                       )
 
 # clean
