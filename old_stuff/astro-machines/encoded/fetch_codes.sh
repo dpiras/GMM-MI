48c3fe98eca63540a27d88f3a34b9906811c9ad7
Davide Piras <d.piras@ucl.ac.uk>
2022-09-28 15:09:33 +0100

    Remove old stuff

--- old_stuff/astro-machines/encoded/fetch_codes.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-wget -N https://www.eso.org/~nsedagha/projects/machines/data/encoded_harps_unique_128d_e182_i1500000.npy
-
--- old_stuff/astro-machines/lists/fetch_lists.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-wget -N https://www.eso.org/~nsedagha/projects/machines/data/harps_metadata_and_labels.csv
