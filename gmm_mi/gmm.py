fixed quad integration of KL computation and added test on this

gmm_mi/gmm.py
@@ -321,7 +321,6 @@ class GMMWithMI(GMM):
         self.MI = KL
         return KL
 
-
     def estimate_MI_quad(self, tol_int=1.49e-8, limit=np.inf):
         """Compute the mutual information (MI) associated with a particular GMM model, 
         using quadrature integration.
@@ -373,14 +372,22 @@ class GMMWithMI(GMM):
         # we create a GMM object to pass to the integral functions
         gmm = GMMWithMI(n_components=self.n_components, weights_init=self.weights_,
                      means_init=self.means_, covariances_init=self.covariances_)
-        entropy_1d_x = integrate.quad(entropy_1d_integrand, -limit, limit,
-                                      args=(gmm, 0), epsabs=tol_int, epsrel=tol_int)[0]
-        entropy_1d_y = integrate.quad(entropy_1d_integrand, -limit, limit,
-                                      args=(gmm, 1), epsabs=tol_int, epsrel=tol_int)[0]
-        KL = entropy_1d_x - entropy_1d_y
+        KL = integrate.quad(integrand_kl_estimate, -limit, limit, args=(gmm) ,epsabs=tol_int, epsrel=tol_int)[0]
         self.MI = KL
         return KL
-    
+
+
+def loglikelihood_1d(x, model, index):
+    assert index == 0 or index == 1, f"Index must be either 0 (x) or 1 (y); found '{index}'"
+    x = np.array(x).reshape(1, 1)
+    return model.score_samples_marginal(x, index)
+
+
+def integrand_kl_estimate(x, model):
+    logp = loglikelihood_1d(x, model, 0)
+    logq = loglikelihood_1d(x, model, 1)
+    return np.exp(logp) * (logp - logq)
+
 
 def log_pdf(y, x, model):
     """Log-likelihood in 2D for a given model

gmm_mi/tests/test_sample.py
@@ -1,5 +1,6 @@
 import numpy as np
 from gmm_mi.mi import EstimateMI
+from gmm_mi.param_holders import MIDistParamHolder
 
 def test_simple():
     # create simple bivariate Gaussian data
@@ -27,9 +28,13 @@ def test_simple():
 
 
 def test_KL_analytic():
-    a = np.random.normal(4, 6, 1000)
-    b = np.random.normal(-0.1, 2, 1000)
+    nu_a, std_a = 0.1, 4
+    nu_b, std_b = 0.1, 5
+    a = np.random.normal(nu_a, std_a, 10000)
+    b = np.random.normal(nu_b, std_b, 10000)
+    analytic_kl = np.log(std_b) - np.log(std_a) - 0.5 * (1 - ((std_a ** 2 + (nu_a - nu_b) ** 2) / std_b ** 2))
     mi_mean, mi_std = EstimateMI().fit(np.column_stack((a, b)), kl=True)
-    analytic_kl = - np.log(6) + np.log(2) - 0.5 * (1 - ((6 ** 2 + (-0.1 - 4) ** 2) / 2 ** 2))
     assert (analytic_kl >= (mi_mean - 2*mi_std)) and (analytic_kl <= (mi_mean + 2*mi_std))
+    mi_mean1, mi_std1 = EstimateMI(mi_dist_params=MIDistParamHolder(MI_method='quad')).fit(np.column_stack((a, b)), kl=True)
+    assert (analytic_kl >= (mi_mean1 - 2 * mi_std1)) and (analytic_kl <= (mi_mean1 + 2 * mi_std1))
 