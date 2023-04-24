1d80fe3b6883aa0a101b04e26d9c1800bfb86b91
Davide Piras <d.piras@ucl.ac.uk>
2023-04-24 11:25:57 +0100

    Reflect accepted version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ PACKAGENAME = 'gmm_mi'
 
 setup(
     name='gmm_mi',
-    version="0.4.3",
+    version="0.5.0",
     author='Davide Piras',
     author_email='dr.davide.piras@gmail.com',
     description='Estimate mutual information distribution with Gaussian mixture models',
